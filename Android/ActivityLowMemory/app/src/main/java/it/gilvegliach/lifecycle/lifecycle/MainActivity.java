f97d47295f7f570fc9d489e6d739527cbec1a47f
Gil Vegliach <gil.vegliach@gmail.com>
2016-01-31 15:12:48 +0100

    Add ActivityLowMemory

---
+++ Android/ActivityLowMemory/app/src/main/java/it/gilvegliach/lifecycle/lifecycle/MainActivity.java
@@ -0,0 +1,72 @@
+package it.gilvegliach.lifecycle.lifecycle;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class MainActivity extends Activity implements FragmentManager.OnBackStackChangedListener {
+    private static final String TAG = "MainActivity";
+    private static final String KEY_ACTIVITY_COUNT = ".activity_count";
+    private int mActivityCount;
+    private int mFragmentCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mActivityCount = getIntent().getIntExtra(KEY_ACTIVITY_COUNT, 1);
+
+        Log.d(TAG, "onCreate(), count: " + mActivityCount);
+
+        FragmentManager fm = getFragmentManager();
+        fm.addOnBackStackChangedListener(this);
+        Fragment f = fm.findFragmentById(R.id.container);
+        if (f == null) {
+            mFragmentCount = 1;
+            f = MainFragment.newInstance(mFragmentCount);
+            fm.beginTransaction().add(R.id.container, f).commit();
+        } else {
+            mFragmentCount = ((MainFragment) f).getCount();
+        }
+
+        TextView tvActivity = (TextView) findViewById(R.id.tv_activity);
+        TextView tvFragment = (TextView) findViewById(R.id.tv_fragment);
+        tvActivity.setText(String.valueOf(mActivityCount));
+        tvFragment.setText(String.valueOf(mFragmentCount));
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy(), count: " + mActivityCount);
+    }
+
+    @Override
+    public void onBackStackChanged() {
+        FragmentManager fm = getFragmentManager();
+        Fragment f = fm.findFragmentById(R.id.container);
+        mFragmentCount = ((MainFragment) f).getCount();
+        TextView tv = (TextView) findViewById(R.id.tv_fragment);
+        tv.setText(String.valueOf(mFragmentCount));
+    }
+
+    public void clickStartActivity(View button) {
+        Intent i = new Intent(this, MainActivity.class);
+        i.putExtra(KEY_ACTIVITY_COUNT, mActivityCount + 1);
+        startActivity(i);
+    }
+
+    public void clickPushFragment(View button) {
+        FragmentManager fm = getFragmentManager();
+        fm.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance(mFragmentCount + 1))
+                .addToBackStack(null)
+                .commit();
+    }
+}
---
+++ Android/ActivityLowMemory/app/src/main/java/it/gilvegliach/lifecycle/lifecycle/MainFragment.java
@@ -0,0 +1,74 @@
+package it.gilvegliach.lifecycle.lifecycle;
+
+import android.app.Fragment;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+
+import static android.widget.FrameLayout.LayoutParams.WRAP_CONTENT;
+import static android.widget.FrameLayout.LayoutParams.MATCH_PARENT;
+
+public class MainFragment extends Fragment {
+    private static final String TAG = "MainFragment";
+    private static final String KEY_COUNT = ".fragment_count";
+    private int mCount;
+
+    public static MainFragment newInstance(int count) {
+        Bundle args = new Bundle(1);
+        args.putInt(KEY_COUNT, count);
+        MainFragment f = new MainFragment();
+        f.setArguments(args);
+        return f;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle args = getArguments();
+        mCount = args.getInt(KEY_COUNT, 1);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        Log.d(TAG, "onCreateView(), count: " + mCount);
+        FrameLayout.LayoutParams lp = new FrameLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT);
+        ImageView iv = new ImageView(getActivity());
+        iv.setLayoutParams(lp);
+        iv.setImageBitmap(BitmapFactory.decodeResource(getResources(), getResId()));
+        return iv;
+    }
+
+    private int getResId() {
+        // R.drawable.large
+        // Jyothis / CC-BY-SA-3.0,
+        // Author: https://commons.wikimedia.org/wiki/User:Jyothis
+        // License: http://creativecommons.org/licenses/by-sa/3.0/legalcode
+        // Original link: https://commons.wikimedia.org/wiki/File:Chess_Large.JPG
+
+        // R.drawable.large2
+        // Editor at Large / CC-BY-SA-2.5
+        // Author: https://commons.wikimedia.org/wiki/User:Editor_at_Large
+        // License: https://creativecommons.org/licenses/by-sa/2.5/legalcode
+        // Original link: https://commons.wikimedia.org/wiki/File:Chocolate_chip_cookie_closeup.jpg
+        return mCount % 2 == 0 ? R.drawable.large
+                : R.drawable.large2;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.d(TAG, "onDestroyView(), count: " + mCount);
+    }
+
+    public int getCount() {
+        return mCount;
+    }
+}
